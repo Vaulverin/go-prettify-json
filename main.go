2b598cdfdfeb7a959920aa432aefe93d77a7c3fe
vaulin.a <vaulin.a@stream.ru>
2017-08-30 19:06:02 +0500

    json prettyfier in progress

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"encoding/json"
 	"bytes"
 )
+var (
+	jsonBegin = byte('{')
+	jsonEnd = byte('}')
+)
 
 func main() {
 	fileContent := getInputFileContent()
@@ -17,10 +21,10 @@ func main() {
 }
 func prettifyContent(content []byte, stopOnError bool, formatTypesToPrettify string) []byte {
 	if strings.Contains(formatTypesToPrettify, "xml") {
-		content = parseXml(content)
+
 	}
 	if strings.Contains(formatTypesToPrettify, "json") {
-		content = parseJson(content)
+		content = prettifyJson(content)
 	}
 	return content
 }
@@ -34,11 +38,51 @@ func getInputFileContent() []byte {
 	}
 	return data
 }
+
+func prettifyJson(content []byte)[]byte {
+	for i:= 0; i < len(content); i++ {
+		beginIndex := bytes.IndexByte(content[i:], jsonBegin)
+		if beginIndex != -1 {
+			endIndex := findJsonEnd(content[beginIndex + 1:])
+			if endIndex != -1 {
+				jsonData := parseJson(content[ beginIndex : endIndex ])
+				if len(jsonData) != 0 {
+					newLine := []byte{'\r', '\n'}
+					jsonData = append(newLine, jsonData...)
+					jsonData = append(jsonData, newLine...)
+					content = bytes.Replace(content, content[ beginIndex : endIndex ], jsonData, -1)
+					i = beginIndex + len(jsonData)
+				}
+			}
+		}
+	}
+	return content
+}
+
 func parseJson(content []byte) []byte {
 	var data bytes.Buffer
 	json.Indent(&data, content, "", "  ")
 	return data.Bytes()
 }
+
+func findJsonEnd(content []byte) int {
+	beginsCount := 1
+	for i, symbol := range content {
+		switch symbol {
+		case jsonBegin:
+			beginsCount++
+		case jsonEnd:
+			beginsCount--
+			if beginsCount == 0 {
+				return i
+			} else if beginsCount < 0 {
+				return -1
+			}
+		}
+	}
+	return -1
+}
+
 func parseXml(content []byte) []byte {
 	return content
 }
